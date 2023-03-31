7259984c2da572b547c8421ecef9517e3b7b8825
Bence Fabian <bence.fabian@codecool.com>
2023-03-31 10:25:41 +0200

    first passing test whoooooooooooooooo

--- backend/src/main/java/com/codecool/logic/Calculator.java
+++ backend/src/main/java/com/codecool/logic/Calculator.java
@@ -5,6 +5,8 @@ import com.codecool.data.user.User;
 public class Calculator {
 
     public double BMICalculator(double weight, double height){
-    return weight / Math.pow(height,2);
+        if (weight <= 0 || height <= 0)
+            throw new IllegalArgumentException("Height and weight must be positive");
+        return weight / Math.pow(height,2);
     }
 }
--- backend/src/test/java/com/codecool/BackendApplicationTests.java
+++ backend/src/test/java/com/codecool/BackendApplicationTests.java
@@ -1,13 +1,18 @@
 package com.codecool;
 
+import com.codecool.logic.Calculator;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void argumentsCannotBeZero() {
+        var calculator = new Calculator();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.BMICalculator(0, 0));
+    }
 
 }
